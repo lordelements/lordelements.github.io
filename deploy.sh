88548259de0afc024eb4c5eb31604e250739238f
Andrian Elemento <andrianelemento41@gmail.com>
2021-11-25 13:20:07 +0800

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:lordelements/lordelements.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/lordelements/final-project-personal-portfolio.git master:gh-pages
+git push -f https://github.com/lordelements/portfolio.github.io.git master:gh-pages
 
 cd -
